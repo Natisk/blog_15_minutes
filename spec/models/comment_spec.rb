cdb6425faefb8177bd2dd88329513178db9c7d7f
Vitaly Matseiko <realnatisk@gmail.com>
2024-09-03 13:36:17 +0200

    specs for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user: user) }
+
+  subject { described_class.new(body: "Text written by a user",
+                                post_id: post.id,
+                                user_id: user.id) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without body" do
+    subject.body = ""
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without user" do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without post" do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
 end
