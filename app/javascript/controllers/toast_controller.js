a001ca8b5984eabb2c6c72be353db214f53ba1b5
Vitaly Matseiko <realnatisk@gmail.com>
2024-09-12 13:20:01 +0200

    modify toastr initial params

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -2,14 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 import toastr from "toastr"
 
 export default class extends Controller {
-  static targets = [ "toast" ]
-
   connect() {
     toastr.options = {
       "closeButton": true,
       "progressBar": true,
-      "positioinClass": "toast-top-right",
-      "timeout": "5000"
+      "positionClass": "toast-top-right",
+      "timeOut": 5000
     }
   }
 
